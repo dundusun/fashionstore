baa56a2190db23e475b5593668e889d69adff9e2
dundusun <dundusun@gmail.com>
2026-05-29 11:20:09 +0530

    point dev proxy to publish 4503

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,9 +5,10 @@ module.exports = function (app) {
   app.use(
     createProxyMiddleware({
       pathFilter: ["/content", "/bin", "/libs", "/apps"],
-      target: "http://localhost:4502",
+      target: "http://localhost:4503",
       changeOrigin: true,
       secure: false,
+      // Publish usually doesn't need Auth, but we can leave it or remove it. Leaving it is safe.
       headers: {
         "Authorization": "Basic YWRtaW46YWRtaW4=",
       },
